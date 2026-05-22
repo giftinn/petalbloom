ffdd0f9b9153af0d0028e3361bee2e08277c2e42
giftin.id <calistanafisaa@gmail.com>
2026-05-22 21:43:49 +0700

    Update main.js

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Hai, Kitten'.split('')
+const text = 'For my dede'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
